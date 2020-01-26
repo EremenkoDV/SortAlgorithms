2b51ec2d467c9348499635e560e9a16529fc07ea
d1550er@gmail.com <d1550er@gmail.com>
2020-01-27 01:00:50 +0300

    Add Insertion Sort

---
+++ Algorithm/InsertionSort.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithm
+{
+    public class InsertionSort<T> : AlgorithmBase<T>
+        where T : IComparable<T>
+    {
+
+        protected override void Sort()
+        {
+            int sortedCount = 0;
+
+            while (sortedCount < Items.Count - 1)
+            {
+                for (int i = sortedCount; i >= 0; i--)
+                {
+                    if (Items[i].CompareTo(Items[i + 1]) == 1)
+                    {
+                        Swap(i, i + 1);
+                        SwapCount++;
+                        ComparisonCount++;
+                    }
+                    else
+                    {
+                        break;
+                    }
+                }
+                sortedCount++;
+            }
+
+
+        }
+
+
+    }
+}
---
+++ AlgorithmTests/InsertionSortTests.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Algorithm;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithm.Tests
+{
+    [TestClass()]
+    public class InsertionSortTests
+    {
+        [TestMethod()]
+        public void SortTest()
+        {
+            // arrange
+            InsertionSort<int> insertion = new InsertionSort<int>();
+
+            Random rnd = new Random();
+            List<int> items = new List<int>();
+            for (int i = 0; i < 10000; i++)
+            {
+                items.Add(rnd.Next(0, 100));
+            }
+            insertion.Items.AddRange(items);
+            List<int> sorted = items.OrderBy(x => x).ToList();
+
+            //cocktail.IsAscending = false;
+
+            // act
+            insertion.SortAndGetSpan();
+
+            // assert
+            for (int i = 0; i < items.Count; i++)
+            {
+                Assert.AreEqual(sorted[i], insertion.Items[i]);
+            }
+
+        }
+    }
+}
